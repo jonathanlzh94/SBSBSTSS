c2ed875b1e355b5452370771c6d68e4df2f21db8
mephet <matthew_kohjm@hotmail.sg>
2018-09-14 10:16:33 +0800

    Create UserMainCode.java

---
+++ S1Q2/UserMainCode.java
@@ -0,0 +1,8 @@
+
+public class UserMainCode {
+	public static void display(String venue) {
+		venue = venue.replace("Stadium", "Ground");
+		System.out.println("Modified Venue");
+		System.out.println(venue);
+	}
+}

3239b536bd0afd28f7ced7e15d90b9c694e69553
mephet <matthew_kohjm@hotmail.sg>
2018-09-14 10:25:30 +0800

    Create Main.java

---
+++ S2Q5/Main.java
@@ -0,0 +1,20 @@
+
+
+import java.util.Scanner;
+
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner sc = new Scanner(System.in);
+		String input = sc.nextLine();
+		String[] strArr = input.split(" ");
+		
+		if(UserMainCode.validatePlayer(strArr[0], strArr[1])) 
+			System.out.println("Valid");
+		else 
+			System.out.println("Invalid");
+	}
+
+}

b0f290a08fc6453210d21b3d47c2d012d97681c5
mephet <matthew_kohjm@hotmail.sg>
2018-09-14 10:19:49 +0800

    Create Main.java

---
+++ S1Q8/Main.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner sc = new Scanner(System.in);
+		
+		System.out.println("Enter the player details");
+		String stat= sc.nextLine();
+		
+		UserMainCode.display(stat);
+	}
+
+}
